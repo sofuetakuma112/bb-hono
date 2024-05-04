f6d03e4f5adee8a960ff58c5f56be884b5f1f8f5
sofuetakuma <kawahagi0620@gmail.com>
2024-05-04 20:50:53 +0900

    first commit

---
+++ app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import db from "@/db";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET!,
+});
